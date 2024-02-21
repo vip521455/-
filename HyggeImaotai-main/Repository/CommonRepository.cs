f474cbe89d92a68f9f5a5b5938bc6296dd066964
vip521455 <160713572+vip521455@users.noreply.github.com>
2024-02-21 15:46:39 +0800

    Add files via upload

---
+++ HyggeImaotai-main/Repository/CommonRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SQLite;
+using System.IO;
+using hygge_imaotai.Entity;
+
+namespace hygge_imaotai.Repository
+{
+    /// <summary>
+    /// 共用或基本的数据库操作
+    /// </summary>
+    internal class CommonRepository
+    {
+        /// <summary>
+        /// 创建订单数据库
+        /// </summary>
+        public static void CreateDatabase()
+        {
+            // 判断数据库文件是否存在
+            if (File.Exists(App.OrderDatabasePath)) return;
+            // 创建数据库连接
+            SQLiteConnection.CreateFile(App.OrderDatabasePath);
+            var sqLiteConnection = new SQLiteConnection(App.OrderDatabaseConnectStr);
+            sqLiteConnection.Open();
+            // 创建表结构
+            var types = new[] { typeof(UserEntity),typeof(ShopEntity),typeof(LogEntity) };
+            DB.Sqlite.CodeFirst.SyncStructure(types);
+        }
+    }
+}
---
+++ HyggeImaotai-main/UserInterface/UserControls/HomeUserControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using hygge_imaotai.Domain;
+
+namespace hygge_imaotai.UserInterface.UserControl
+{
+    /// <summary>
+    /// HomeUserControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class HomeUserControl : System.Windows.Controls.UserControl
+    {
+        public HomeUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void DonateButton_OnClick(object sender, RoutedEventArgs e)
+            => Link.OpenInBrowser("http://donate.lisok.cn/#");
+    }
+}
